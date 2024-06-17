690da85be59fd1990feaf19ae5aee22ab711021e
RajputRaj2023 <162285240+RajputRaj2023@users.noreply.github.com>
2024-06-17 16:37:41 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountId = 123455
+let accountEmail = "raj9200@gmail.com"
+var accountPassword = "123"
+acountCity = "gwalior"
+
+//accountId = 3333
+/*
+prefer to not use var  because  of issue  i block scope and function
+*/
+console.log(accountId);
+accountEmail="nainsi@8827gmail.com"
+accountPassword="raj@1234567"
+acountCity ="gwalior";
+let accountState;
+console.table([accountId,accountEmail,accountPassword,acountCity,accountState])

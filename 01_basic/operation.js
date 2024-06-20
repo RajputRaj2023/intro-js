1068281df12ad9471014943dc56fbb0c0b81f70e
RajputRaj2023 <162285240+RajputRaj2023@users.noreply.github.com>
2024-06-20 07:52:58 +0000

    comparision operators

---
+++ 01_basic/operation.js
@@ -0,0 +1,14 @@
+let value =3 
+let negvalue = -value
+console.log(negvalue)
+
+let str1 ="Raj"
+let str2 ="rajput"
+ let str3 = str1 + str2
+ console.log(str3)
+
+//  console.log("1"+ 2)
+//  console.log(2 + "1")
+//  console.log("1" +2 +3)
+//  console.log(2 + 3 +"1")
+console.log(+ true)

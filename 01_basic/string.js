2e38b351ca382c46d988ebc7335a0ad46ca5ec35
RajputRaj2023 <162285240+RajputRaj2023@users.noreply.github.com>
2024-06-21 04:03:22 +0000

    string method

---
+++ 01_basic/string.js
@@ -0,0 +1,22 @@
+const name ="Raj";
+const valueCount= 50;
+
+//console.log( name + valueCount + "value")
+//console.log(`Hello this is my ${name} and my value is ${valueCount}`)
+const gameName = new String("mariogame-raj-chandan-sourav-deepak-arvind-nainsi")
+//console.log(gameName.toUpperCase())
+//console.log(gameName.slice("i"))
+const newString =gameName.substring(0,2)
+//console.log(newString);
+
+
+const othergame = gameName.trim();
+//console.log(othergame);
+
+const url = "https://rajrajput.com/raj%20rajput";
+//console.log(url.replace('%20', '-'))
+//console.log(url.includes("raj"))
+console.table(gameName.split("-"))
+
+
+
